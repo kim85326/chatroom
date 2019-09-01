274d351f872b298fafeb3d0181e6125a9315fd74
elaine <kim85326@gmail.com>
2019-09-01 19:43:49 +0800

    Updates

---
+++ precache-manifest.2b8699f88669f0d0c1bc0a0e52f76335.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865e440ce7a174b2b932ed80a9040c3b",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "86e561c5e0c46f943b5b",
+    "url": "/chatroom/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "608ce0b5ce1c2d721854",
+    "url": "/chatroom/static/css/main.c6e9200f.chunk.css"
+  },
+  {
+    "revision": "86e561c5e0c46f943b5b",
+    "url": "/chatroom/static/js/2.28d193e9.chunk.js"
+  },
+  {
+    "revision": "608ce0b5ce1c2d721854",
+    "url": "/chatroom/static/js/main.489f69b1.chunk.js"
+  },
+  {
+    "revision": "a2612f4e52728bca94c5",
+    "url": "/chatroom/static/js/runtime~main.1722e496.js"
+  },
+  {
+    "revision": "712279cb6613ae5c2bed72904f4fa428",
+    "url": "/chatroom/static/media/avatar1.712279cb.svg"
+  },
+  {
+    "revision": "661bde11a652489e90b7436536434aba",
+    "url": "/chatroom/static/media/avatar2.661bde11.svg"
+  },
+  {
+    "revision": "a7defcfbcdc0fa9e8d80948a1fc4729c",
+    "url": "/chatroom/static/media/avatar3.a7defcfb.svg"
+  },
+  {
+    "revision": "be549aaf44a24005c84cdd95b87577ca",
+    "url": "/chatroom/static/media/avatar4.be549aaf.svg"
+  },
+  {
+    "revision": "077b472a396ccfad0dba4bbebcf24c70",
+    "url": "/chatroom/static/media/avatar5.077b472a.svg"
+  },
+  {
+    "revision": "7fc779aee3f4f0ceece4360a42eed11f",
+    "url": "/chatroom/static/media/emoji.7fc779ae.svg"
+  },
+  {
+    "revision": "ef174e457dac23aab1e03e87c8852326",
+    "url": "/chatroom/static/media/file.ef174e45.svg"
+  },
+  {
+    "revision": "ed1395c94378a016f0c676cb2bb10c2d",
+    "url": "/chatroom/static/media/logo.ed1395c9.svg"
+  },
+  {
+    "revision": "ddd24800a5aa03f4f99e274943d1b061",
+    "url": "/chatroom/static/media/logout.ddd24800.svg"
+  },
+  {
+    "revision": "ac782c005ed5e7f925e2421a3cd65ee5",
+    "url": "/chatroom/static/media/photo.ac782c00.svg"
+  },
+  {
+    "revision": "661fd5b7ac5c5fe3af01af1f07334331",
+    "url": "/chatroom/static/media/search.661fd5b7.svg"
+  }
+]);
